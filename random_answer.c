e361dc588795d7d2645368e871c0595f27ee1417
Radket27 <103642613+Radket27@users.noreply.github.com>
2022-04-14 15:46:12 +0200

    add file random_answer.c
    add file random_answer.c

---
+++ random_answer.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int losowa(){
+    srand(time(NULL));
+    int liczba = rand() % 5;
+    return liczba;
+}
+
+char* nazwa(int liczba){
+    char* przepowiednia;
+    switch(liczba){
+        case 0:
+        przepowiednia = "Yes";
+        break;
+        case 1:
+        przepowiednia = "No";
+        break;
+        case 2:
+        przepowiednia = "Maybe";
+        break;
+        case 3:
+        przepowiednia = "Try again";
+        break;
+        case 4:
+        przepowiednia = "You need to look deeper";
+        break;
+    }
+    return przepowiednia;
+
+}
+
+int zart(int czas){
+    printf("Your data are analysing\n");
+    sleep(czas);
+    printf("Is almost done\n");
+    sleep(czas);
+    printf("Answer is almost done\n");
+    sleep(czas);
+    return 0;
+}
+
+
+int main(){
+    char* pytanie;
+    printf("Write your question ('_' use this symbol if you have more then 1 word in your question):\n");
+    scanf("%s",pytanie);
+    printf("Now I'm looking for answer: %s\n",pytanie);
+    int a1 = losowa();
+
+    zart(a1);
+
+    char* los = nazwa(a1);
+    printf("The answer is: %s\n",los);
+
+    return 0;
+}
